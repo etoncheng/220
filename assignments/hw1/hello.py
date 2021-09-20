e58fdfb3e72ca2522d4b748d7b94862d7a8d0dfa
Eton Cheng <chengel@g.cofc.edu>
2021-09-20 10:28:10 -0400

    completed homework 1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -2,13 +2,15 @@
 Name: Eton Cheng
 hello.py
 
-Problem: this program illustrates a hello function
+Problem: write a program displaying "hello, world!"
 
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 """
 
+
 def hello():
     print("hello, world!")
+
 
 hello()
