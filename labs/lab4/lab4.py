b621674831c1716a9fad8b42d0f0de46154b83d3
Eton Cheng <chengel@g.cofc.edu>
2021-09-21 16:22:21 -0400

    completed lab 4

--- labs/lab4/lab4.py
+++ labs/lab4/lab4.py
@@ -1,9 +1,15 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Eton Cheng
+lab4.py
+
+Problem: Graphics and Accumulation Practice
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
 """
 
 from graphics import *
+import math
 
 
 def squares():
@@ -25,51 +31,103 @@ def squares():
     height = 400
     win = GraphWin("Lab 4", width, height)
 
-    # number of times user can move circle
+    # number of times user can move square
     num_clicks = 5
 
     # create a space to instruct user
     inst_pt = Point(width / 2, height - 10)
-    instructions = Text(inst_pt, "Click to move circle")
+    instructions = Text(inst_pt, "Click to move square")
     instructions.draw(win)
 
-    # builds a circle
-    shape = Circle(Point(50, 50), 20)
+    # builds a square
+    shape = Rectangle(Point(50, 50), Point(20, 20))
     shape.setOutline("red")
     shape.setFill("red")
     shape.draw(win)
 
-    # allows the user to click multiple times to move the circle
+    # allows the user to click multiple times to move the square
     for i in range(num_clicks):
         p = win.getMouse()
-        c = shape.getCenter()  # center of circle
+        c = shape.getCenter()  # center of square
 
-        # move amount is distance from center of circle to the
+        # move amount is distance from center of square to the
         # point where the user clicked
         dx = p.getX() - c.getX()
         dy = p.getY() - c.getY()
         shape.move(dx, dy)
+        shape = Rectangle(Point(50, 50), Point(20, 20))
+        shape.setOutline("red")
+        shape.setFill("red")
+        shape.draw(win)
 
     win.getMouse()
     win.close()
 
 
+squares()
+
+
 def rectangle():
     """
     This program displays information about a rectangle drawn by the user.
-    Input: Two mouse clicks for the opposite corners of a rectangle.
+    Input: Mouse clicks for the opposite corners of a rectangle.
     Output: Draw the rectangle.
-         Print the perimeter and area of the rectangle.
-    Formulas: area = (length)(width)   and    perimeter = 2(length+width)
     """
-    pass
+    # Creates a graphical window
+    win = GraphWin("Draw a rectangle", 400, 400)
+
+    p1 = win.getMouse()
+    p1.draw(win)
+    p2 = win.getMouse()
+    p2.draw(win)
+    p3 = win.getMouse()
+    p3.draw(win)
+    p4 = win.getMouse()
+    p4.draw(win)
+
+    rectangle = Polygon(p1, p2, p3, p4)
+    rectangle.setFill("green")
+    rectangle.setOutline("blue")
+    rectangle.draw(win)
+
+    win.getMouse()
+    win.close()
 
 
-def main():
-    squares()
-    # rectangle()
-    # circle()
-    # pi2()
+rectangle()
 
 
-main()
+def circle():
+    # Creates a graphical window
+    win = GraphWin("Draw a rectangle", 400, 400)
+    p1 = win.getMouse()
+    p1.draw(win)
+    p2 = win.getMouse()
+    p2.draw(win)
+
+    x1 = p1.getX()
+    x2 = p1.getX()
+    y1 = p2.getY()
+    y2 = p2.getY()
+
+    radius = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
+    win.getMouse()
+    win.close()
+
+circle()
+
+
+def pi2():
+    pi = 0
+    n = 4
+    d = 1
+    for d in range(1, 12):
+        a = 2 * (d % 2) - 1
+        pi += a * n / d
+        d += 2
+
+    print(pi)
+
+
+pi2()
