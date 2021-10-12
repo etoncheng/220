4591d7def13853a0591552177515df91106a6460
Eton Cheng <chengel@g.cofc.edu>
2021-10-12 17:47:11 -0400

    completed homework 7

---
+++ assignments/hw7/weighted_average.py
@@ -0,0 +1,115 @@
+"""
+Name: Eton Cheng
+weighted_average.py
+
+Problem: student average
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
+
+def main():
+    student1 = ["Billy Bother", 20, 89, 30, 94, 50, 82]
+    student2 = ["Oh No", 30, 52, 60, 75]
+    student3 = ["Hermione Heffalump", 40, 93, 60, 97]
+    student4 = ["Too Bad", 90, 95, 10, 87, 20, 94]
+    student5 = ["Kurt Kidd", 20, 88, 30, 82, 40, 76, 10, 99]
+    print(student1)
+    print(student2)
+    print(student3)
+    print(student4)
+    print(student5)
+
+    grades = student1[2::2]
+    weights = student1[1::2]
+
+    def average(grades, weights):
+        weights_shares = []
+        for i in range(0, len(weights)):
+            weights_shares.append(weights[i] / sum(weights))
+
+        weights_grades = []
+        for i in range(0, len(grades)):
+            weights_grades.append(grades[i] * weights_shares[i])
+
+        weighted_average = sum(weights_grades)
+
+        return weighted_average
+
+    print("Billy Bother's average:", average(grades, weights))
+
+    grades = student2[2::2]
+    weights = student2[1::2]
+
+    def average(grades, weights):
+        weights_shares = []
+        for i in range(0, len(weights)):
+            weights_shares.append(weights[i] / sum(weights))
+
+        weights_grades = []
+        for i in range(0, len(grades)):
+            weights_grades.append(grades[i] * weights_shares[i])
+
+        weighted_average = sum(weights_grades)
+
+        return weighted_average
+
+    print("Oh No's average:", average(grades, weights))
+
+    grades = student3[2::2]
+    weights = student3[1::2]
+
+    def average(grades, weights):
+        weights_shares = []
+        for i in range(0, len(weights)):
+            weights_shares.append(weights[i] / sum(weights))
+
+        weights_grades = []
+        for i in range(0, len(grades)):
+            weights_grades.append(grades[i] * weights_shares[i])
+
+        weighted_average = sum(weights_grades)
+
+        return weighted_average
+
+    print("Hermione Heffalump's average:", average(grades, weights))
+
+    grades = student4[2::2]
+    weights = student4[1::2]
+
+    def average(grades, weights):
+        weights_shares = []
+        for i in range(0, len(weights)):
+            weights_shares.append(weights[i] / sum(weights))
+
+        weights_grades = []
+        for i in range(0, len(grades)):
+            weights_grades.append(grades[i] * weights_shares[i])
+
+        weighted_average = sum(weights_grades)
+
+        return weighted_average
+
+    print("Too Bad's average:", average(grades, weights))
+
+    grades = student5[2::2]
+    weights = student5[1::2]
+
+    def average(grades, weights):
+        weights_shares = []
+        for i in range(0, len(weights)):
+            weights_shares.append(weights[i] / sum(weights))
+
+        weights_grades = []
+        for i in range(0, len(grades)):
+            weights_grades.append(grades[i] * weights_shares[i])
+
+        weighted_average = sum(weights_grades)
+
+        return weighted_average
+
+    print("Kurt Kidd's average:", average(grades, weights))
+
+
+main()
